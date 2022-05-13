fe63a76b72376246e0cb6f43735b3f14728e9c0d
MichaCrivelli <crivelli.micha@gmail.com>
2022-05-14 00:06:20 +0200

    Player Movement and mouselook

---
+++ Assets/Scripts/Player/Player_Mouselook.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Mouselook : MonoBehaviour
+{
+    [SerializeField, Range(0, 10)] float sensitivityX;
+    [SerializeField, Range(0, 10)] float sensitivityY;
+    Quaternion originalCamRotation;
+    Quaternion originalPlayerRotation;
+    float rotationX = 0;
+    float rotationY = 0;
+
+    Transform player;
+
+    [Header("Clamping")]
+    [SerializeField, Range(-360f, 360f)] float minimumX;
+    [SerializeField, Range(-360f, 360f)] float maximumX;
+    [SerializeField, Range(-90f, 90f)] float minimumY;
+    [SerializeField, Range(-90f, 90f)] float maximumY;
+
+
+    void Start()
+    {
+        player = transform.parent;
+        originalCamRotation = transform.localRotation;
+        originalPlayerRotation = player.localRotation;
+    }
+
+    void Update()
+    {
+        rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        rotationX = Mathf.Clamp(rotationX, minimumX, maximumX);
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+        Quaternion quaternionX = Quaternion.AngleAxis(rotationX, Vector3.up);
+        Quaternion quaternionY = Quaternion.AngleAxis(rotationY, -Vector3.right);
+        transform.localRotation = originalCamRotation * quaternionY;
+        player.localRotation = originalPlayerRotation * quaternionX;
+    }
+
+
+}
---
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float runSpeed;
+    [SerializeField] float airSpeed;
+    [SerializeField] float jumpForce;
+    [SerializeField] bool moveInAir;
+    [SerializeField] bool isGrounded;
+    bool isMoving;
+
+    Vector3 inputDir;
+    Vector3 forward;
+    RaycastHit groundHit;
+    Rigidbody m_rigidbody;
+    CapsuleCollider m_collider;
+    float rayLength;
+
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+        m_collider = GetComponent<CapsuleCollider>();
+        rayLength = m_collider.bounds.extents.y + 0.2f;
+        Debug.Log(rayLength);
+    }
+
+    void Update()
+    {
+        
+        inputDir = (forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal")).normalized;
+
+        // multiply inputDir with correct speed
+        if (isGrounded)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                inputDir *= runSpeed;
+            }
+            else
+            {
+                inputDir *= speed;
+            }
+        } else { inputDir *= airSpeed; }
+
+        Debug.DrawLine(transform.position, transform.position + forward * 10, Color.red, Time.deltaTime);
+
+    }
+
+    private void FixedUpdate()
+    {
+        // check for the ground and set the forward vector parallel to it
+        if (Physics.Raycast(transform.position, -transform.up, out groundHit,rayLength))
+        {
+            forward = Vector3.Cross(transform.right, groundHit.normal);
+            if (groundHit.transform.gameObject.tag == "Ground")
+            {
+                isGrounded = true;
+            }
+        }
+        else
+        {
+            forward = transform.forward;
+            isGrounded = false;
+        }
+
+        // movement
+        if (inputDir != Vector3.zero)
+        {
+            if (!isGrounded)
+            {
+                if (moveInAir)
+                {
+                    m_rigidbody.MovePosition(transform.position + inputDir * Time.deltaTime);
+                    isMoving = true;
+                }
+                else { isMoving = false; }
+            }
+            else
+            {
+                isMoving = true;
+                m_rigidbody.MovePosition(transform.position + inputDir * Time.deltaTime);
+            }
+        } else { isMoving = false; }
+
+        // jumping when on the ground
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            Debug.Log("jump");
+            if (isMoving)
+            {
+                m_rigidbody.AddForce(Vector3.up * jumpForce + inputDir.normalized*jumpForce, ForceMode.Impulse);
+            }
+            else
+            {
+                m_rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+
+}
